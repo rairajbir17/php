41bf8d2087bae4b3ccd97b210b7eff243e7d0f60
Rajbir Kaur <rajbir21001003@gmail.com>
2017-06-13 01:03:01 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Example</title>
+    </head>
+    <body>
+    
+        <?php
+        echo " welcome to our project";
+            echo "Hi, I'm a PHP script!";
+        ?>
+    </body>
+</html>
