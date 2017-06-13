d68ab14b2c5d5bb44b99370601b1b0846e430ad8
Rajbir Kaur <rajbir21001003@gmail.com>
2017-06-13 01:22:06 -0700

    Create index.php

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
         <title>Example</title>
     </head>
     <body>
-    
+        <H1> Welcome to Heroku </h1>
         <?php
-        echo " welcome to our project";
+        echo " welcome to our project\n";
             echo "Hi, I'm a PHP script!";
         ?>
     </body>
